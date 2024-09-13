aa1bc22dc206bc2c5485ca99087c0e9588ac1283
mbsoares <mbsoares.infor@gmail.com>
2024-09-12 22:04:33 -0300

    aula 7

--- app/src/main/java/com/example/petapp/DashBoardActivity.java
+++ app/src/main/java/com/example/petapp/DashBoardActivity.java
@@ -1,6 +1,9 @@
 package com.example.petapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +19,20 @@ public class DashBoardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_dash_board);
         // pegando os dados do pai
         String login = (String) getIntent().getSerializableExtra("login");
+        Toast.makeText(this, "Bem-vindo: " + login,
+                Toast.LENGTH_SHORT).show();
+        setTitle("DashBoard");
     }
 
+    public void cadastroPet(View view) {
+        // abre a Activity de cadastro
+        Intent intent = new Intent(this,CadastroPetActivity.class);
+        startActivity(intent);
+    }
+
+    public void listagemPet(View view) {
+        // abre a Activity de listagem
+        Intent intent = new Intent(this,ListagemPetActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/petapp/LoginActivity.java
+++ app/src/main/java/com/example/petapp/LoginActivity.java
@@ -22,7 +22,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        setTitle("Login");
     }
 
     public void logar(View view) {
